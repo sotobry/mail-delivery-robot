2809c472a7520f28ee57c3b5706add51027517c4
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2019-11-16 15:01:13 -0800

    Converts the list of roads to a data structure that, for each place, it tells what can be reached from there.

--- mail-delivery-robot.js
+++ mail-delivery-robot.js
@@ -17,3 +17,21 @@ const roads =
     "Shop-Town Hall"
   ];
 
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+
+  function addEdge(from, to) {
+    if (graph[from] == null) graph[from] = [to];
+    else graph[from].push(to);
+  }
+
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
