bed38dfb21c88ba97691b8ea4203563255b2e0f6
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2019-11-16 14:40:17 -0800

    Adds the roads constant representing the village's graph.

---
+++ mail-delivery-robot.js
@@ -0,0 +1,19 @@
+/* The village of Meadowfield isn't very big. It consists of 11 places with 14 roads between them. It can be described with this array of roads:*/
+const roads =
+  [
+    "Alice's House-Bob's House",
+    "Alice's House-Cabin",
+    "Alice's House-Post Office",
+    "Bob's House-Town Hall",
+    "Daria's House-Ernie's House",
+    "Daria's House-Town Hall",
+    "Ernie's House-Grete's House",
+    "Grete's House-Farm",
+    "Grete's House-Shop",
+    "Marketplace-Farm",
+    "Marketplace-Post Office",
+    "Marketplace-Shop",
+    "Marketplace-Town Hall",
+    "Shop-Town Hall"
+  ];
+
