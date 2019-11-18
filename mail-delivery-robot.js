70cdc61d7eea31bfdd6b629f2cf9a12158b0ca86
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2019-11-18 09:42:12 -0800

    Finishes 1.0 version of project

--- mail-delivery-robot.js
+++ mail-delivery-robot.js
@@ -53,6 +53,19 @@ class VillageState {
       return new VillageState(destination, parcels);
     }
   }
+
+  static random(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+      let address = randomPick(Object.keys(roadGraph));
+      let place;
+      do {
+        place = randomPick(Object.keys(roadGraph));
+      } while (place == address);
+      parcels.push({ place, address });
+    }
+    return new VillageState("Post Office", parcels);
+  }
 }
 
 let place = "Post Office";
@@ -61,13 +74,118 @@ let parcels = [{ place: "Post Office", address: "Alice's House" }];
 let first = new VillageState(place, parcels);
 let next = first.move("Alice's House");
 
-console.log(next.place);
-console.log(next.parcels);
-console.log(first.place);
-console.log(first.parcels);
+// console.log(next.place);
+// console.log(next.parcels);
+// console.log(first.place);
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      // console.log(`Done in ${turn} turns.`);
+      return turn;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    // console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length)
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+const mailRoute = ["Alice's House", "Cabin", "Alice's House", "Bob's House", "Town Hall", "Daria's House", "Ernie's House", "Grete's House", "Shop", "Grete's House", "Farm", "Marketplace", "Post Office"];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) memory = mailRoute;
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+
+function findAvgTurns(times, robot, memory) {
+  const TIMES = times;
+  let total = 0;
+  for (let i = 0; i < TIMES; i++) {
+    total += runRobot(VillageState.random(), robot, memory);
+  }
+  return Math.round(total / TIMES);
+}
+
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address)
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
 
 
+//console.log(findAvgTurns(1000, goalOrientedRobot, []));
+//On average the goalOrientedRobot finishes in 15 turns
+//About 4.6 times faster than randomRobot!!
+//About 1.2 times faster than routeRobot!!
+//console.log(findAvgTurns(100, routeRobot, mailRoute));
+//On average the routeRobot finishes in 18 turns
+//About 3.8 times faster than randomRobot!!
+//console.log(findAvgTurns(100000, randomRobot));
+//On average the randomRobot finishes in 69 turns
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  findAvgTurns(100, robot1, memory1, robot2, memory2);
+  let total1 = 0, total2 = 0;
+
+  for (let i = 0; i < 100; i++) {
+    let state = VillageState.random();
+    total1 += runRobot(state, robot1, memory1);
+    total2 += runRobot(state, robot2, memory2);
+  }
+  let avg1 = Math.round(total1 / 100);
+  let avg2 = Math.round(total2 / 100);
+
+  console.log(`The average number of steps robot 1 took per task was ${avg1}`);
+  console.log(`The average number of steps robot 2 took per task was ${avg2}`);
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
 
 
+function goalOrientedRobot2({ place, parcels }, route) {
+  if (route.length == 0) {
+    for (let parcel of parcels) {
+      if (parcel.place != place) {
+        if (route.length > findRoute(roadGraph, place, parcel.place).length) {
+          route = findRoute(roadGraph, place, parcel.place);
+        }
+      }
+    }
+    for (let parcel of parcels) {
+      route = route.concat(findRoute(roadGraph, place, parcel.address));
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+console.log(findAvgTurns(10000, goalOrientedRobot2, []));
 
 
